3f1dc58facd4ee1a16cf82967cbfb14b0e70d69b
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-26 23:36:46 +0100

    Wissel tussen spawn

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -6,26 +6,49 @@ public class FoodSpawner : MonoBehaviour {
     public Transform JunkFood;
     public Transform HealthyFood;
     public List<GameObject> spawnPoints = new List<GameObject>();
+    private List<GameObject> freeSpawnPoints; 
 
     public GameObject[] JunkFoods;
     public GameObject[] HealthyFoods;
 
 	// Use this for initialization
 	void Start () {
-		
+        freeSpawnPoints = new List<GameObject>(spawnPoints);
 	}
 
-    Vector3 SelectSpawnPoint(){
+    void SelectSpawnPoint(){
         int randomIndex = Random.Range(0, spawnPoints.Count);
-        return spawnPoints[randomIndex].transform.position;
+        Debug.Log(randomIndex);
+   //     Vector3 spawnPoint =  spawnPoints[randomIndex].transform.position;
+   //     Debug.Log(CheckIfSpawnIsFree(spawnPoint));
+
+        foreach (var spwnpnt in spawnPoints)
+        {
+            if (CheckIfSpawnIsFree(spwnpnt.transform.position))
+            {
+                Debug.Log("free: " + spwnpnt);
+                SpawnNewFood(spwnpnt);
+                return;
+            }
+        }
+ 
     }
 
-    void SpawnNewFood(){
+
+    void SpawnNewFood( GameObject spawnPoint){
         Transform food = (JunkFoods.Length > HealthyFoods.Length) ? HealthyFood : JunkFood;
 
-        Instantiate(food, SelectSpawnPoint(), Quaternion.identity);
+        Instantiate(food, spawnPoint.transform.position, Quaternion.identity);
         UpdateFoodLists();
     }
+
+    bool CheckIfSpawnIsFree(Vector3 spawnPoint){
+        
+        var spawnPointColliders = Physics.OverlapSphere(spawnPoint, 1);
+        Debug.Log(spawnPointColliders.Length);
+        return !(spawnPointColliders.Length > 1);
+    }
+
 
     void UpdateFoodLists(){
         JunkFoods = GameObject.FindGameObjectsWithTag("JunkFood");
@@ -36,7 +59,7 @@ public class FoodSpawner : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown("space")){
             Debug.Log(spawnPoints.Count);
-            SpawnNewFood();
+            SelectSpawnPoint();
         }
 	}
 }
