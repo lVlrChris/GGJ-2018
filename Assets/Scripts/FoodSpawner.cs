b1cc4400cadb55d680c8cd5583dd3a87e63b0eee
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-26 22:28:21 +0100

    Spawn random frikandels

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour {
-
+    public Transform frikandel;
+    public List<GameObject> spawnPoints = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+    Vector3 SelectSpawnPoint(){
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        return spawnPoints[randomIndex].transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown("space")){
+            Debug.Log(spawnPoints.Count);
+            Instantiate(frikandel, SelectSpawnPoint(), Quaternion.identity);
+        }
 	}
 }
