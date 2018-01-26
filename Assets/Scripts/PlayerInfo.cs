64bfcfd58e16b065bdd594776a0faf0ae35d1e2c
Niels van Dam <nielsvdam@hotmail.com>
2018-01-27 00:13:40 +0100

    Diet enum in playerinfo

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerInfo : MonoBehaviour {
 
     public int playerIndex;
     public new string name;
+    public Diet diet;
 
 	// Use this for initialization
 	void Start () {
