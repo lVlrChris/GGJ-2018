47d9ab1eb9e596bd51d8adfb36045a9296d09c56
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-27 00:12:16 +0100

    Auto stash before merge of "development" and "origin/development"
    troep

--- Assets/Scripts/PlayerLoot.cs
+++ Assets/Scripts/PlayerLoot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLoot : MonoBehaviour {
+    public int JunkCount, HealthCount;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +14,19 @@ public class PlayerLoot : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        var collisionTag = col.gameObject.tag;
+        Debug.Log(collisionTag);
+        if (collisionTag == "JunkFood"){
+            JunkCount++;
+            Destroy(col.gameObject);
+        }           
+        else if( collisionTag == "HealthyFood")
+        {
+            HealthCount++;
+            Destroy(col.gameObject);
+        }
+    }
 }
