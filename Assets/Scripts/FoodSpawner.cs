a1859db358390cebaa25714b520a3e936926eaf6
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-26 22:34:41 +0100

    spawn

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour {
     public Transform frikandel;
     public List<GameObject> spawnPoints = new List<GameObject>();
+
+    private GameObject[] JunkFoods;
+    private GameObject[] HealthyFoods;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,12 +18,23 @@ public class FoodSpawner : MonoBehaviour {
         int randomIndex = Random.Range(0, spawnPoints.Count);
         return spawnPoints[randomIndex].transform.position;
     }
+
+    void SpawnNewFood(){
+        Instantiate(frikandel, SelectSpawnPoint(), Quaternion.identity);
+
+    }
+
+    void UpdateFoodLists(){
+        JunkFoods = GameObject.FindGameObjectsWithTag("JunkFoods");
+        HealthyFoods = GameObject.FindGameObjectsWithTag("HealthyFoods");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("space")){
             Debug.Log(spawnPoints.Count);
-            Instantiate(frikandel, SelectSpawnPoint(), Quaternion.identity);
+            SpawnNewFood();
         }
 	}
 }
