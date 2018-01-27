b2cd75d90c5667a4c6a2da1b44054b5e8b411d7f
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-27 18:32:38 +0100

    Auto stash before merge of "development" and "origin/development"

--- Assets/Scripts/RoundTimer.cs
+++ Assets/Scripts/RoundTimer.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 
 public class RoundTimer : MonoBehaviour {
     public float gameTime = 90f;
+<<<<<<< Updated upstream
     private bool startedEndTimer = false;
+=======
+    public AudioSource audioSource;
+    public AudioClip mainMusic;
+    public AudioClip endMusic;
+
+    bool endMusicPlaying = false;
+
+>>>>>>> Stashed changes
     public Text GameTimer;
     private GameManager gameManager;
 	// Use this for initialization
 	void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        audioSource.Play();
 	}
 	
 	// Update is called once per frame
@@ -19,12 +29,24 @@ public class RoundTimer : MonoBehaviour {
         int min = Mathf.FloorToInt(gameTime / 60);
         int sec = Mathf.FloorToInt(gameTime % 60);
         GameTimer.text = min.ToString("00") + ":" + sec.ToString("00");
+<<<<<<< Updated upstream
         if (gameTime < 7 && !startedEndTimer)
         {
             StartCoroutine(AnimateSeconds(5));
             startedEndTimer = true;
         }
         if (gameTime < 1){
+=======
+        if(gameTime <= 5 && !endMusicPlaying) {
+            endMusicPlaying = true;
+            audioSource.Stop();
+            audioSource.clip = endMusic;
+            audioSource.Play();
+
+        }
+        if (gameTime <= 0){
+
+>>>>>>> Stashed changes
             print("end game");
             gameManager.EndGame();
         }
