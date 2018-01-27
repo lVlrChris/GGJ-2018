cf845637c66eda1d90f65b6263366389e243c8ed
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-27 02:42:06 +0100

    Auto stash before merge of "development" and "origin/development"

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -8,15 +8,26 @@ public class PlayerInfo : MonoBehaviour {
     public int playerIndex;
     public new string name;
     public Diet diet;
-    public float percentage;
+    public float junkPercentage = 0f;
+    public float healthyPercentage = 0f;
+
+    private PlayerLoot playerLoot;
 
 	// Use this for initialization
 	void Start () {
-		
+        playerLoot = GetComponent<PlayerLoot>();
 	}
+
+    void UpdatePercentage(){
+        junkPercentage = playerLoot.JunkCount / (playerLoot.JunkCount + playerLoot.HealthCount) * 100;
+        healthyPercentage =  playerLoot.HealthCount / (playerLoot.JunkCount + playerLoot.HealthCount) * 100;
+        Debug.Log("junk" + junkPercentage);
+        Debug.Log("health" + healthyPercentage);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        UpdatePercentage();
 	}
 }
