8f543766b4e5072f204484cb3e6d45f1cea5b184
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2018-01-27 02:50:03 +0100

    percentage

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class PlayerInfo : MonoBehaviour {
 
@@ -10,6 +10,8 @@ public class PlayerInfo : MonoBehaviour {
     public Diet diet;
     public float junkPercentage = 0f;
     public float healthyPercentage = 0f;
+
+    public Text scoreText;
 
     private PlayerLoot playerLoot;
 
@@ -23,7 +25,7 @@ public class PlayerInfo : MonoBehaviour {
         healthyPercentage =  playerLoot.HealthCount / (playerLoot.JunkCount + playerLoot.HealthCount) * 100;
         Debug.Log("junk" + junkPercentage);
         Debug.Log("health" + healthyPercentage);
-
+        scoreText.text = "Junk: " + junkPercentage + "% " + "Healthy: " + healthyPercentage + "%"; 
     }
 	
 	// Update is called once per frame
