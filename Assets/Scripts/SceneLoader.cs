71f3ac15bb08a09b15ae6df625b43432050e57e8
lVlrChris <christian-boer@hotmail.com>
2018-01-27 15:23:15 +0100

    Menu controller support

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string gamesceneName;
+    public string menuSceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Loading " + gamesceneName + "...");
+        SceneManager.LoadScene(gamesceneName);
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Loading " + menuSceneName + "...");
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
