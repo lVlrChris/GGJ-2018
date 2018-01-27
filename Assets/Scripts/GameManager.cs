2227e8f17b014a2023bbd8771927b9c0350e4173
Niels van Dam <nielsvdam@hotmail.com>
2018-01-27 05:51:16 +0100

    mainmenu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject fastFoodPlayer;
+    public GameObject healthyPlayer;
+
+    public Transform spawnPosP1;
+    public Transform spawnPosP2;
+
+    void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	
+    public void StartGame()
+    {
+        SceneManager.LoadScene("NielsScene");
+        SpawnPlayers();
+    }
+
+    private void SpawnPlayers()
+    {
+        GameObject p1 = Instantiate(fastFoodPlayer, spawnPosP1.position, spawnPosP1.rotation) as GameObject;
+        p1.GetComponent<PlayerInfo>().playerIndex = 1;
+        GameObject p2 = Instantiate(healthyPlayer, spawnPosP2.position, spawnPosP2.rotation) as GameObject;
+        p2.GetComponent<PlayerInfo>().playerIndex = 2;
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/RoundTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundTimer : MonoBehaviour {
+    public float gameTime = 30f;
+    
+    public Text GameTimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameTime -= Time.deltaTime;
+        GameTimer.text = Mathf.Round(gameTime).ToString();
+        if(gameTime <= 0){
+            GameTimer.text = "KLAAAAAR";
+        }
+	}
+}
