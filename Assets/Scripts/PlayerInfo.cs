fa7a3fd874f7400dd8b41c63e9ac69056053d8fb
Niels van Dam <nielsvdam@hotmail.com>
2018-01-28 12:50:21 +0100

    dingen

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -15,23 +15,13 @@ public class PlayerInfo : MonoBehaviour {
     public float aimPercentage = 0f;
     public float firedShots = 0f;
     public float landedShots = 0f;
-
-    public Text junkText;
-    public Text healthText;
-
-    public Transform UIPanel;
-    private Slider healthySlider, junkSlider;
+   
 
     private PlayerLoot playerLoot;
 
 	// Use this for initialization
 	void Start () {
         playerLoot = GetComponent<PlayerLoot>();
-        UIPanel = GameObject.Find("PanelP" + playerIndex).transform;
-        healthySlider = UIPanel.Find("HealthSlider").GetComponent<Slider>();
-        junkSlider = UIPanel.transform.Find("JunkSlider").GetComponent<Slider>();
-        junkText = UIPanel.transform.Find("JunkPercentage").GetComponent<Text>();
-        healthText = UIPanel.transform.Find("HealthPercentage").GetComponent<Text>();
     }
 
     void UpdatePercentage(){
@@ -39,8 +29,6 @@ public class PlayerInfo : MonoBehaviour {
         healthyPercentage =  playerLoot.HealthCount / (playerLoot.JunkCount + playerLoot.HealthCount) * 100;
         //Debug.Log("junk" + junkPercentage);
         //Debug.Log("health" + healthyPercentage);
-        healthySlider.value = (healthyPercentage / 100);
-        junkSlider.value = junkPercentage / 100;
     }
 	
 	// Update is called once per frame
