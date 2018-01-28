669d2c06bfa6ad55197369e29d3db02190023519
lVlrChris <christian-boer@hotmail.com>
2018-01-28 11:57:19 +0100

    Blinklights on pickups

---
+++ Assets/Scripts/BlinkLight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkLight : MonoBehaviour {
+    public float minRange;
+    public float maxRange;
+    public float speed;
+    public bool increasing = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currentRange = GetComponent<Light>().range;
+
+        if (increasing)
+        {
+            GetComponent<Light>().range += speed;
+
+            if (currentRange >= maxRange)
+            {
+                increasing = false;
+            }
+        }
+
+        if (!increasing)
+        {
+            GetComponent<Light>().range -= speed;
+
+            if (currentRange <= minRange)
+            {
+                increasing = true;
+            }
+        }
+        
+	}
+}
